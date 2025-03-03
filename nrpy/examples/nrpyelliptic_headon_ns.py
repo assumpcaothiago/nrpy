8719a19a33c31d93eb281d0d7515c562165f9ef8
Thiago Assumpção <assumpcaothiago@gmail.com>
2025-03-03 12:05:12 -0600

    nrpy/examples/nrpyelliptic_headon_ns.py: set gcc as default compiler.

--- nrpy/examples/nrpyelliptic_headon_ns.py
+++ nrpy/examples/nrpyelliptic_headon_ns.py
@@ -316,6 +316,7 @@ Makefile_helpers.output_CFunctions_function_prototypes_and_construct_Makefile(
     project_dir=project_dir,
     project_name=project_name,
     exec_or_library_name=project_name,
+    CC="gcc",  # For compatiblity with 3d interpolation
 )
 print(
     f"Finished! Now go into project/{project_name} and type `make` to build, then ./{project_name} to run."
