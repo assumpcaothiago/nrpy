0163e2117ad01ae01b1338cfa85426ff3eaa73f3
Thiago Assumpção <tassumpo@uwm.edu>
2026-02-23 13:31:44 -0600

    infrastructures/manga/__init__.py: formatting.

--- nrpy/infrastructures/manga/__init__.py
+++ nrpy/infrastructures/manga/__init__.py
@@ -1,7 +1,8 @@
 from . import (
+    BHaH_get_metric_extrinsic_curvature,
     BHaH_setup,
     manga_radial_initial_data,
     rescaledvU_from_vCartU,
     T4munu_from_primitives,
-    u4Ut
+    u4Ut,
 )
