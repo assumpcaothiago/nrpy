7fe90d2c4dedfc37615d391bda28d57b7edb138c
Thiago Assumpção <tassumpo@uwm.edu>
2026-02-24 17:02:54 -0600

    nrpy/infrastructures/manga/__init__.py: add two new functions: BHaH_evolve() and BHaH_checkpoint_and_diagnostics()

--- nrpy/infrastructures/manga/__init__.py
+++ nrpy/infrastructures/manga/__init__.py
@@ -1,6 +1,8 @@
 from . import (
+    BHaH_evolve,
     BHaH_get_metric_extrinsic_curvature,
     BHaH_setup,
+    BHaH_checkpoint_and_diagnostics,
     manga_radial_initial_data,
     rescaledvU_from_vCartU,
     T4munu_from_primitives,
